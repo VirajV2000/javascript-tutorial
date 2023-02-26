0c43175e856804b3f6663496b475199d52d6c1de
Viraj <121506903+VirajV2000@users.noreply.github.com>
2023-02-26 10:27:22 +0000

    var_let_const

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-console.log("This is a variable");
-let a=67
-console.log(a)
-a="harry"
+console.log("this is variable types")
+// var a=9;
+// var a="harry"
+let a=9;
+ a="harry"
+const b="viraj"
+console.log(b)
 console.log(a)
